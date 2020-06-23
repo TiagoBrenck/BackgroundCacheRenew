9fb6849d6927f9b73eebe39081cf48b42c16b49a
Tiago Brenck <v-tibre@microsoft.com>
2020-06-23 11:13:58 -0700

    WebAPI test sample

---
+++ WebAPI/Controllers/TodoListController.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// The same code for the controller is used in both chapters of the tutorial. 
+// In the first chapter this is just a protected API (ENABLE_OBO is not set)
+// In this chapter, the Web API calls a downstream API on behalf of the user (OBO)
+#define ENABLE_OBO
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class TodoListController : Controller
+    {
+        public TodoListController(ITokenAcquisition tokenAcquisition)
+        {
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        readonly ITokenAcquisition _tokenAcquisition;
+
+        static readonly ConcurrentBag<TodoItem> TodoStore = new ConcurrentBag<TodoItem>();
+
+        /// <summary>
+        /// The Web API will only accept tokens 1) for users, and 
+        /// 2) having the access_as_user scope for this API
+        /// </summary>
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+            string owner = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return TodoStore.Where(t => t.Owner == owner).ToList();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async void Post([FromBody]TodoItem todo)
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+            string owner = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            string ownerName;
+#if ENABLE_OBO
+            // This is a synchronous call, so that the clients know, when they call Get, that the 
+            // call to the downstream API (Microsoft Graph) has completed.
+            try
+            {
+                ownerName = CallGraphApiOnBehalfOfUser().GetAwaiter().GetResult();
+                string title = string.IsNullOrWhiteSpace(ownerName) ? todo.Title : $"{todo.Title} ({ownerName})";
+                TodoStore.Add(new TodoItem { Owner = owner, Title = title });
+            }
+            catch (MsalException ex)
+            {
+                HttpContext.Response.ContentType = "text/plain";
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await HttpContext.Response.WriteAsync("An authentication error occurred while acquiring a token for downstream API\n" + ex.ErrorCode + "\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Response.ContentType = "text/plain";
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await HttpContext.Response.WriteAsync("An error occurred while calling the downstream API\n" + ex.Message);
+            }
+#endif
+
+        }
+
+        public async Task<string> CallGraphApiOnBehalfOfUser()
+        {
+            string[] scopes = { "user.read" };
+
+            // we use MSAL.NET to get a token to call the API On Behalf Of the current user
+            try
+            {
+                string accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+                dynamic me = await CallGraphApiOnBehalfOfUser(accessToken);
+                return me.UserPrincipalName;
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                _tokenAcquisition.ReplyForbiddenWithWwwAuthenticateHeader(scopes, ex);
+                return string.Empty;
+            }
+        }
+
+        private static async Task<dynamic> CallGraphApiOnBehalfOfUser(string accessToken)
+        {
+            // Call the Graph API and retrieve the user's profile.
+            GraphServiceClient graphServiceClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) => {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                return Task.FromResult(0);
+            }));
+
+            User graphUser = await graphServiceClient.Me.Request().GetAsync();
+            
+            return graphUser;
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using DataAccessLayer;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.TokenCacheProviders;
+using Microsoft.Identity.Web.TokenCacheProviders.Distributed;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CacheDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TokenCacheDbConnStr")));
+            services.AddScoped<IMsalAccountActivityRepository, MsalAccountActivityRepository>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddProtectedWebApi(Configuration);
+
+            services.AddProtectedWebApiCallsProtectedWebApi(Configuration)
+                    .AddDistributedTokenCaches();
+
+            services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options => 
+            {
+                Configuration.Bind("AzureAd", options);
+                var existingHandler = options.Events.OnTokenValidated; // handler from Microsoft.Identity.Web
+
+                options.Events.OnTokenValidated = async context =>
+                {
+                    // Execute Microsoft.Identity.Web event handler first
+                    await existingHandler(context).ConfigureAwait(false);
+
+                    // Since OnTokenValidated will be called multiple times, it is good to filter what operation will trigger the insert of 
+                    // MsalAccountActivity, otherwise the DB could get overwhelmed and the app gets slow
+                    if (context.HttpContext.Request.Method == "POST" && context.Request.Path == "/api/todolist")
+                    {
+                        var tokenAcquisition = context.HttpContext.RequestServices.GetRequiredService<ITokenAcquisition>();
+                        var cacheProvider = context.HttpContext.RequestServices.GetRequiredService<IMsalTokenCacheProvider>();
+                        var appOptions = new ConfidentialClientApplicationOptions();
+
+                        Configuration.Bind("AzureAd", appOptions);
+
+                        var app = ConfidentialClientApplicationBuilder.Create(appOptions.ClientId)
+                                    .WithAuthority(options.Authority)
+                                    .WithClientSecret(appOptions.ClientSecret)
+                                    .Build();
+
+                        await cacheProvider.InitializeAsync(app.UserTokenCache);
+
+                        var bearerToken = context.HttpContext.Items["JwtSecurityTokenUsedToCallWebAPI"] as JwtSecurityToken;
+
+                        var accounts = await app.GetAccountsAsync().ConfigureAwait(false);
+
+                        if (bearerToken != null && accounts.Count() > 0)
+                        {
+                            var accountActivity = new MsalAccountActivity(accounts.First(), bearerToken.RawSignature);
+                            var repo = context.HttpContext.RequestServices.GetRequiredService<IMsalAccountActivityRepository>();
+                            await repo.UpsertActivity(accountActivity);
+                        }
+                    }
+                };
+            });
+
+            services.AddDistributedSqlServerCache(options =>
+            {
+                /*
+                    dotnet tool install --global dotnet-sql-cache
+                    dotnet sql-cache create "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MY_TOKEN_CACHE_DATABASE;Integrated Security=True;" dbo TokenCache    
+                */
+                options.ConnectionString = Configuration.GetConnectionString("TokenCacheDbConnStr");
+                options.SchemaName = "dbo";
+                options.TableName = "TokenCache";
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                // Since IdentityModel version 5.2.1 (or since Microsoft.AspNetCore.Authentication.JwtBearer version 2.2.0),
+                // PII hiding in log files is enabled by default for GDPR concerns.
+                // For debugging/development purposes, one can enable additional detail in exceptions by setting IdentityModelEventSource.ShowPII to true.
+                // Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles(); // For the wwwroot folder
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
